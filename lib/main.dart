cdf7b2e1e47f869acb19ad0097ab34a306c4705f
Ujjwal <ujjwalcpj123@gmail.com>
2026-08-20 00:37:29 +0530

    #tic_tac_toe - creates skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -54,18 +54,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Color borderColor = Colors.blue;
+  Color backgroundColor = Colors.black;
 
   @override
   Widget build(BuildContext context) {
@@ -76,47 +66,138 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SizedBox(
+          width: 300,
+          height: 300,
+          child: Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: backgroundColor,
+                        child: Cell(),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: backgroundColor,
+                        child: Cell(),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: backgroundColor,
+                        child: Cell(shouldShowCross: true),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: backgroundColor,
+                        child: Cell(),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: backgroundColor,
+                        child: Cell(),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: backgroundColor,
+                        child: Cell(),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: backgroundColor,
+                        child: Cell(),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: backgroundColor,
+                        child: Cell(),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: backgroundColor,
+                        child: Cell(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Positioned(
+                left: 95,
+                child: Container(width: 10, height: 300, color: borderColor),
+              ),
+              Positioned(
+                right: 95,
+                child: Container(width: 10, height: 300, color: borderColor),
+              ),
+              Positioned(
+                top: 95,
+                child: Container(width: 300, height: 10, color: borderColor),
+              ),
+              Positioned(
+                bottom: 95,
+                child: Container(width: 300, height: 10, color: borderColor),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Cell extends StatefulWidget {
+  bool shouldShowCross = false;
+  Cell({this.shouldShowCross = false});
+
+  @override
+  State<Cell> createState() => _CellState();
+}
+
+class _CellState extends State<Cell> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          widget.shouldShowCross = !widget.shouldShowCross;
+        });
+      },
+      child: Container(
+        width: 100,
+        height: 100,
+        alignment: Alignment.center,
+        child: Text(
+          widget.shouldShowCross ? 'X' : 'O',
+          style: TextStyle(color: Colors.white, fontSize: 40),
+        ),
+      ),
     );
   }
 }
